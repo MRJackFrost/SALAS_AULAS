4f6d20771b1a65d2748df8d0089e70ca76632e0a
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-05-15 22:26:54 -0300

    Update MediadealunosFixa.cpp

--- MediadealunosFixa.cpp
+++ MediadealunosFixa.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//criando a classe de sala
 class Sala {
 public:
     int qtdsala = 6;
@@ -10,7 +10,7 @@ public:
 };
 
 int main() {
-
+    //definindo os valores fixos de alunos
     Sala nossasala;
     nossasala.qtdaluno[0] = 35;
     nossasala.qtdaluno[1] = 4;
@@ -20,15 +20,19 @@ int main() {
     nossasala.qtdaluno[5] = 30;
     nossasala.qtdsala = 6;
     int i = 0;
+    
+    //mostrando pro usuario as salas com a quantidade e pegando a media
    while(i < nossasala.qtdsala ){
    int numsala = i+1;
    cout << "A sala: " << numsala << " Quantidade de alunos: " << nossasala.qtdaluno[i]<<endl;
    nossasala.media = nossasala.media + nossasala.qtdaluno[i];
    i = i+1;
 }
+     //calculo da media
     nossasala.media = nossasala.media/nossasala.qtdsala;
     cout << "Media de alunos por sala: " << nossasala.media<< endl;
     i = 0;
+    //mostrando pro usuario as salas que estao acima da media
     while(i <nossasala.qtdsala){
         
         if(nossasala.qtdaluno[i] >= nossasala.media){
